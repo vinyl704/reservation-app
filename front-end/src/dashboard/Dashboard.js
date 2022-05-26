6be7eb1f3142219e86b6a8f72feef6817dd61ba5
vinyl704 <kingvinyl@gmail.com>
2022-05-26 11:52:51 -0400

    staged files

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { listReservations } from "../utils/api";
+import useQuery from "../utils/useQuery"
 import ErrorAlert from "../layout/ErrorAlert";
-
+import {today,next,previous} from "../utils/date-time"
+import {useHistory} from 'react-router';
 /**
  * Defines the dashboard page.
  * @param date
  *  the date for which the user wants to view reservations.
  * @returns {JSX.Element}
  */
+
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
-
+  const query = useQuery()
+  const dateQuery = query.get("date")
+  const [pageDate, setPageDate] = useState(dateQuery ? dateQuery : date);
   useEffect(loadDashboard, [date]);
 
   function loadDashboard() {
+    const date = pageDate;
     const abortController = new AbortController();
     setReservationsError(null);
     listReservations({ date }, abortController.signal)
@@ -22,12 +28,41 @@ function Dashboard({ date }) {
       .catch(setReservationsError);
     return () => abortController.abort();
   }
+ const history = useHistory();
 
+ 
+
+  const nextDateHandler = () => {
+    
+    setPageDate(()=>next(pageDate));
+    
+    history.push(`/dashboard?date=${pageDate}`);
+    loadDashboard(date = pageDate)
+
+  };
+
+  const previousDateHandler = () => {
+
+   
+    setPageDate(()=>previous(pageDate));
+    history.push(`/dashboard?date=${pageDate}`);
+  };
+
+  const todayHandler = () => {
+    setPageDate(()=>today());
+    history.push(`/dashboard?date=${pageDate}`);
+
+  };
   return (
     <main>
       <h1>Dashboard</h1>
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for date</h4>
+        <div className="btn-group">
+          <button onClick={previousDateHandler} name="previous" className="btn">Previous Day</button>
+          <button onClick={todayHandler} name="today" className="btn">Today</button>
+          <button onClick={nextDateHandler} name="next" className="btn">Next Day</button>
+        </div>
       </div>
       <ErrorAlert error={reservationsError} />
       {JSON.stringify(reservations)}

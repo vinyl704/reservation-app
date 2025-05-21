b07ef5c4105ee9c3205cb607035bda693769d75d
M. Barber <36747278+vinyl704@users.noreply.github.com>
2025-05-21 11:33:49 -0400

    Update knexfile.js
    updated knexfile

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -55,7 +55,12 @@ module.exports = {
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL,
+    connection: {
+      connectionsString:DATABASE_URL,
+      ssl:{
+      rejectUnauthorized: true,
+    ca: fs.readFileSync("./ca.pem").toString(),
+      },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },

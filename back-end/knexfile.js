813a17ad53ea76491217d41e5ec6b18517f3f919
M. Barber <36747278+vinyl704@users.noreply.github.com>
2025-05-31 17:49:00 -0400

    Update knexfile.js
    replaced with version from local work

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -1,26 +1,48 @@
+/**
+ * Knex configuration file.
+ *
+ * You will not need to make changes to this file.
+ */
 
 require("dotenv").config();
+//const client = require("./src/db/service")
 const path = require("path");
-
+const fs = require("fs");
+//console.log(client.config)
 const {
   DATABASE_URL = "postgresql://postgres@localhost/postgres",
- 
+  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  POSTGRES_CA = "",
   DEBUG,
 } = process.env;
 
+config = {
+  DB_SSL: fs.readFileSync("ca.pem")
+}
+
 module.exports = {
   development: {
-    client: "postgresql",
+  client: "postgres",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL,
+    connection: {
+    connectionString: DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
+  },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    debug: !!DEBUG,
-  },
+}
+  ,
   test: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
@@ -65,5 +87,5 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
     debug: DEBUG,
-  }
-}
+  },
+};

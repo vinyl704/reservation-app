6ded8be90db2268d556991a38a65c3fb2d49b7eb
M. Barber <36747278+vinyl704@users.noreply.github.com>
2025-05-31 17:47:05 -0400

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -48,22 +48,22 @@ module.exports = {
   production: {
     client: "postgresql",
     pool: { min: 1, max: 5 },
-    connection: DATABASE_URL,
-    /*{
-    
+    connection: {
+    connectionString: DATABASE_URL,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false,
-  },*/
+    ssl: config['DB_SSL'] ? { rejectUnauthorized: false } : false,
+  },
+
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-    //debug: !!DEBUG,
+    debug: DEBUG,
   }
 }
